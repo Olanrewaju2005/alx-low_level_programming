781a1b0dc33e5a4504257275a152c614beb20fe6
Olanrewaju2005 <davidolanrewaju04@gmail.com>
2023-08-10 14:03:50 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  *malloc_checked - function alllocates memory using malloc
+  *@b: number of bytes of tpo be allocated
+  *Return: pointer to the space allocated
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+	
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+### This is the readme of this directory
+# we will  be dealing with more on malloc, free
